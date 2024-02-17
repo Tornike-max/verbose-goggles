f27bce8b9abf4ec76103ff76fd0bcb1b99b2a706
Tornike-max <84152983+Tornike-max@users.noreply.github.com>
2024-02-18 02:04:27 +0400

    implement clerk webhooks

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -9,10 +9,17 @@ interface MongooseConnection {
 
 let cached: MongooseConnection = (global as any).mongoose;
 
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
-  if (!MONGODB_URL) throw new Error("Error! Missing Mongodb url");
+  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
 
   cached.promise =
     cached.promise ||
